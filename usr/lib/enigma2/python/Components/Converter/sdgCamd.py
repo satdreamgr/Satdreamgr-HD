a3f6e6cb19ee295005becc958b7f405868bbd139
Athanasios Oikonomou <athoik@gmail.com>
2014-07-18 22:14:59 +0300

    initial commit

---
+++ usr/lib/enigma2/python/Components/Converter/sdgCamd.py
@@ -0,0 +1,64 @@
+from enigma import iServiceInformation
+from Components.Converter.Converter import Converter
+from Components.Element import cached
+from Tools.Directories import fileExists
+import os
+
+class sdgCamd(Converter, object):
+
+    def __init__(self, type):
+        Converter.__init__(self, type)
+
+    @cached
+    def getText(self):
+        service = self.source.service
+        info = service and service.info()
+        camd = ''
+        camdlist = None
+        serlist = None
+        if not info:
+            return ''
+        if fileExists('/etc/init.d/softcam'):
+            try:
+                camdlist = os.popen('/etc/init.d/softcam info')
+            except:
+                pass
+                
+        elif fileExists('/etc/init.d/cardserver'):
+            try:
+                serlist = os.popen('/etc/init.d/cardserver info')
+            except:
+                pass
+
+        else:
+            return
+        if serlist is not None:
+            try:
+                cardserver = ''
+                for current in serlist.readlines():
+                    cardserver = current
+
+                serlist.close()
+            except:
+                pass
+
+        else:
+            cardserver = ''
+        if camdlist is not None:
+            try:
+                emu = ''
+                for current in camdlist.readlines():
+                    emu = current
+
+                camdlist.close()
+            except:
+                pass
+
+        else:
+            emu = ''
+        return '%s %s' % (cardserver.split('\n')[0], emu.split('\n')[0])
+
+    text = property(getText)
+
+    def changed(self, what):
+        Converter.changed(self, what)
